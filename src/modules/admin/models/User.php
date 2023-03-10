0a6978d7ea40e05b87658c88b6ecdad099ae3f1e
Elias Luhr <e.luhr@herzogkommunikation.de>
2023-03-10 12:06:35 +0100

    Update user model
    Added label to describe user

--- src/modules/admin/models/User.php
+++ src/modules/admin/models/User.php
@@ -5,8 +5,20 @@ namespace dmstr\oauth\modules\admin\models;
 /**
  * @inheritdoc
  *
+ * @property-read string $label
+ *
  * /!\ Just for giiant generation. Do not use /!\
 */
 class User extends \Da\User\Model\User
 {
+
+    /**
+     * Label to uniquely describe the user
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->username;
+    }
+
 }
